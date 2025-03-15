ae322359022375afc0692ab104e7793ba67aec80
DmitriyZimin <demaxru@gmail.com>
2025-03-15 13:19:59 +0300

    Добавление расчета арифметических операций для двух чисел

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,38 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Случайное число от 0 до 1: " + Math.random());
+        System.out.println("Введите первое число: ");
+        int firstNumber = numberFromInput();
+        System.out.println("Введите второе число: ");
+        int secondNumber = numberFromInput();
+        System.out.println("Сумма чисел равна: " + numberSum(firstNumber, secondNumber));
+        System.out.println("Разность чисел равна: " + numberDifference(firstNumber, secondNumber));
+        System.out.println("Произведение чисел равно: " + numberMultiplication(firstNumber, secondNumber));
+        System.out.println("Частное чисел равно: " + numberDivision(firstNumber, secondNumber));
+    }
+
+    public static int numberFromInput() {
+        return new Scanner(System.in).nextInt();
+    }
+
+    //Сумма чисел
+    public static int numberSum(int firstNumber, int secondNumber) {
+        return firstNumber + secondNumber;
+    }
+
+    //Разность чисел
+    public static int numberDifference(int firstNumber, int secondNumber) {
+        return firstNumber - secondNumber;
+    }
+
+    //Произведение чисел
+    public static int numberMultiplication(int firstNumber, int secondNumber) {
+        return firstNumber * secondNumber;
+    }
+
+    //Частное чисел
+    public static double numberDivision(int firstNumber, int secondNumber) {
+        return (double) firstNumber / secondNumber;
     }
 }
