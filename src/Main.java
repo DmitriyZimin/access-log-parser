cc6f9bc62ac0dd1e150d101bf3210de6ca40bf10
DmitriyZimin <demaxru@gmail.com>
2025-05-06 11:22:23 +0300

    Курсовой проект. Задание по теме Циклы

--- src/Main.java
+++ src/Main.java
@@ -1,41 +1,34 @@
+import java.io.File;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите текст и нажмите <Enter>: ");
-        String text = new Scanner(System.in).nextLine();
-        System.out.println("Длина текста: " + text.length());
-        System.out.println("Введите первое число: ");
-        int firstNumber = numberFromInput();
-        System.out.println("Введите второе число: ");
-        int secondNumber = numberFromInput();
-        System.out.println("Сумма чисел равна: " + numberSum(firstNumber, secondNumber));
-        System.out.println("Разность чисел равна: " + numberDifference(firstNumber, secondNumber));
-        System.out.println("Произведение чисел равно: " + numberMultiplication(firstNumber, secondNumber));
-        System.out.println("Частное чисел равно: " + numberDivision(firstNumber, secondNumber));
+        filePath();
     }
 
-    public static int numberFromInput() {
-        return new Scanner(System.in).nextInt();
-    }
+    public static void filePath() {
+        Scanner scanner = new Scanner(System.in);
+        int attempts = 0;
 
-    //Сумма чисел
-    public static int numberSum(int firstNumber, int secondNumber) {
-        return firstNumber + secondNumber;
-    }
+        while (true) {
+            System.out.print("Введите путь к файлу:");
 
-    //Разность чисел
-    public static int numberDifference(int firstNumber, int secondNumber) {
-        return firstNumber - secondNumber;
-    }
+            String path = scanner.nextLine();
+            File file = new File(path);
 
-    //Произведение чисел
-    public static int numberMultiplication(int firstNumber, int secondNumber) {
-        return firstNumber * secondNumber;
-    }
+            boolean fileExists = file.exists();
+            boolean isDirectory = file.isDirectory();
 
-    //Частное чисел
-    public static double numberDivision(int firstNumber, int secondNumber) {
-        return (double) firstNumber / secondNumber;    
+            if (!fileExists && !isDirectory) {
+                System.out.println("Указан путь к несуществующему файлу");
+                continue;
+            } else if (isDirectory) {
+                System.out.println("Указан путь к папке");
+                continue;
+            }
+
+            attempts++;
+            System.out.println("Путь указан верно. Это файл номер " + attempts);
+        }
     }
 }
