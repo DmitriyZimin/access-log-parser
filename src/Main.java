56776457e54a49573bc11b152b25b4ab550688fc
DmitriyZimin <demaxru@gmail.com>
2025-06-20 22:41:35 +0300

    Выделение класса с исключением в отдельный файл

--- src/Main.java
+++ src/Main.java
@@ -73,11 +73,3 @@ public class Main {
         }
     }
 }
-
-class StringTooLongException extends RuntimeException {
-    private static final String MESSAGE = "Найдена строка длиной больше 1024 символов!";
-
-    public StringTooLongException() {
-        super(MESSAGE);
-    }
-}
